6208aa95bb100f97f640154869d64d5eb9815fd2
Farhanasherin-tk <farhanasherin8808@gmail.com>
2025-02-12 11:47:31 +0530

    Add files via upload

---
+++ Arithematic.java
@@ -0,0 +1,18 @@
+public class Arithematic{
+public static void main(String[]args){
+int x=15;
+int y=10;
+int sum;
+int sub;
+int mul;
+int div;
+sum=x+y;
+System.out.println(sum);
+sub=x-y;
+System.out.println(sub);
+mul=x*y;
+System.out.println(mul);
+div=x/y;
+System.out.println(div);
+}
+}
---
+++ Sum.java
@@ -0,0 +1,13 @@
+public class Sum{
+public static void main(String[]args){
+int x,y,z,o;
+x=10+3;
+System.out.println(x);
+y=10-3;
+System.out.println(y);
+z=10*2;
+System.out.println(z);
+o=10/2;
+System.out.println(o);
+}
+}
